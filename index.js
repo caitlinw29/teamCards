a34bce7cfab618df81b97317226a83a4b8af7a10
Caitlin Wall <caitlinwall96@gmail.com>
2022-01-17 14:13:02 -0500

    Structure inquire prompts so that questions are asked in order before moving along to next function

--- index.js
+++ index.js
@@ -1,59 +1,119 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const generateHTML = require('./src/generateHTML');
+const Employee = require('./lib/Employee');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
+const teamMembers = [];
 
-const teamName = [
-  {
-      type: 'input',
-      name: 'team',
-      message: 'What is the name of your team?',
-  },
-];
-//   {
-//       type: 'input',
-//       name: 'manager',
-//       message: 'What is your name?',
-//   },
-//   {
-//       type: 'input',
-//       name: 'managerID',
-//       message: 'What is your ID?',
-//   },
-//   {
-//       type: 'input',
-//       name: 'managerEmail',
-//       message: 'What is your email?',
-//   },
-//   {
-//       type: 'input',
-//       name: 'managerOffice',
-//       message: 'What is your office number?',
-//   },
-// ];
 
-// const menu = [
-//   {
-//     type: 'list',
-//     name: 'choiceMenu',
-//     choices: ["Add an engineer", "Add an intern", "I'm finished!"],
-//     message: 'Who would you like to add to the team?',
-// },
-// {
-//   type: "input",
-//   name: "engineerName",
-//   message: "What is this engineer's name?",
-//   when: (answers) => answers.choiceMenu === "Add an engineer",
-// },
-// {
-//   type: "input",
-//   name: "engineerName",
-//   message: "What is this engineer's name?",
-//   when: (answers) => answers.choiceMenu === "Add an intern",
-// },
-// ];
+
+let teamName;
+
+function startTeamBuilding() {
+  inquirer
+      .prompt([
+        {
+            type: 'input',
+            name: 'team',
+            message: 'What is the name of your team?',
+        },
+        {
+          type: 'input',
+          name: 'manager',
+          message: "What is the manager's name?",
+        },
+        {
+          type: 'input',
+          name: 'managerID',
+          message: "What is the manager's ID?",
+        },
+        {
+          type: 'input',
+          name: 'managerEmail',
+          message: "What is the manager's email?",
+        },
+        {
+          type: 'input',
+          name: 'managerOffice',
+          message: "What is the manager's office number?",
+        },
+      ])
+      .then((data) => {
+        teamName = data.team;
+        const manager = new Manager(data.manager, data.managerID, data.managerEmail, data.managerOffice);
+        teamMembers.push(manager);
+        chooseTeamMember();        
+        
+      });
+}
+
+
+function chooseTeamMember(){
+  inquirer
+    .prompt([
+      {
+        type: 'list',
+        name: 'choiceMenu',
+        choices: ["Add an engineer", "Add an intern", "I'm finished!"],
+        message: 'Who would you like to add to the team?',
+      },
+    ])
+    .then((choice) => {
+      switch(choice.choices){
+        case "Add an engineer":
+          makeEngineer();
+          break;
+        case "Add an intern":
+          makeIntern();
+          break;
+        default:
+          createProfiles();
+      }
+    });
+}
+
+function makeEngineer(){
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "engineerName",
+        message: "What is this engineer's name?",
+      },
+    ])
+    .then((engineer) => {
+
+    })
+
+}
+
+function makeIntern(){
+  inquirer
+    .prompt([ 
+      {
+        type: "input",
+        name: "internName",
+        message: "What is this intern's name?",
+      },
+      {
+        type: "input",
+        name: "internID",
+        message: "What is this intern's ID?",
+      },
+    ])
+
+}
+
+function createProfiles(){
+  writeToFile("./dist/index.html", generateHTML(
+    teamName,
+    teamMembers
+  ));
+}
+
+
 
 //Set up function to write answers in file
 function writeToFile(fileName, data) {
@@ -62,29 +122,5 @@ function writeToFile(fileName, data) {
   );
 }
 
-//inquire the questions, then put answers in a writeToFile function aimed at the index.html
-function getTeam() {
-  inquirer
-      .prompt(teamName)
-      .then((data) => {
-          const teamName = data.team;
-          // getTeamProfiles();
-          writeToFile("./dist/index.html", generateHTML(
-            teamName,
-          ));
-      });
-}
-
-function getTeamProfiles(){
-  getName();
-  getId();
-  getEmail();
-}
 // Function call to initialize app
-getTeam();
-
-
-
-
-
-//set up function blocks in classes. import said classes. Then have ONE menu, with if statements. If engineer, run Engineer.getId() etc 
+startTeamBuilding();
